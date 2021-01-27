c7987d90345564c6029a3a259e66878de75bdec3
Allison Usher <allyjay317@gmail.com>
2021-01-26 20:03:58 -0800

    added preferred_statistics table migration

---
+++ data/migrations/20210126200319_create-preferred_statistics.js
@@ -0,0 +1,15 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('preferred_statistics', (tbl) => {
+    tbl.string('okta_email').notNullable();
+    tbl
+      .integer('stat_id')
+      .references('id')
+      .inTable('statistics')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('preferred_statistics');
+};
