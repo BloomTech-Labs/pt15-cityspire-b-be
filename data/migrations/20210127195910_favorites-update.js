042204b7d0a87002ce18e6243049f149c0a5a69c
Allison Usher <allyjay317@gmail.com>
2021-01-27 21:03:26 -0800

    updated favorites migration

---
+++ data/migrations/20210127195910_favorites-update.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.dropTable('favorites').createTable('favorites', (tbl) => {
+    tbl.increments();
+    tbl.string('email').notNullable();
+    tbl.integer('zip_code').default(-1);
+    tbl.string('city').default('default');
+    tbl.string('state').default('default');
+  });
+};
+
+exports.down = function (knex) {};
